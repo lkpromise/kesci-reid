70b89b5c47380cbdbc13fad77e1268c388b6551e
kai <liukai__2018@163.com>
2019-10-31 09:23:19 +0800

    使用focal_loss替换交叉熵，添加新的配置文件，生成了第三天提交结果。

--- kesci-master/data/datasets/kesci_all.py
+++ kesci-master/data/datasets/kesci_all.py
@@ -87,8 +87,9 @@ class Kesci_all(BaseImageDataset):
             img_name = os.listdir(dir_path)
             for i in img_name:
                 img_path = osp.join(dir_path,i)
+                ## 生成结果的时候才能用下面这一行
                 id = i.split('.')[0]
-                # id = random.randint(4768,8600)
+                #id = random.randint(4768,8600)
                 camid = random.randint(0,6)
                 dataset.append((img_path,id,camid))
         return dataset
